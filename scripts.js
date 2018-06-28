7afa6f579250d9184ae92a937b44a4fc40258c8f
TrystanKeohnae <40602346+TrystanKeohnae@users.noreply.github.com>
2018-06-28 09:09:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  $("button").click(function(){
+  $("#hide").click(function(){
     alert("Hello from my page");
     $("iframe").hide();
+  });
+  $("#show").click(function(){
+       $("iframe").show();
   });
 });
