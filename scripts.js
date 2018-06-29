821bd864807e316faf2ad9732cf6a25026dbf7cd
TrystanKeohnae <40602346+TrystanKeohnae@users.noreply.github.com>
2018-06-29 09:04:33 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#hide").click(function(){
-    alert("Sup fam, you aint seeing the sounds anymore Ha!");
+    alert("Sup fam, you aint seeing the Flames anymore Ha!");
     $("iframe").hide();
   });
   $("#show").click(function(){
